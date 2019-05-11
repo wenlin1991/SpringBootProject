5002a5199954632029edf15952e044be504bd8a3
wenlin <linwen1991@gmail.com>
2019-05-11 14:18:09 -0700

    Inital commit: add project.

---
+++ PasswordService/src/main/java/wenlin/demo/PasswordService/PasswordServiceApplication.java
@@ -0,0 +1,13 @@
+package wenlin.demo.PasswordService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordServiceApplication.class, args);
+	}
+
+}
